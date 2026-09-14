ab2a9811e03542b13936a953ccea170411ba5d16
Mohd Shadaan <Shadaan0019vstar@gmail.com>
2026-09-14 18:22:10 +0530

    updated dockerfile for Shayri-club frontend

--- Shayri-club/src/Apis/axiosInstance.js
+++ Shayri-club/src/Apis/axiosInstance.js
@@ -1,9 +1,31 @@
+// import axios from "axios";
+
+// // Automatically detect environment
+// const API_URL = import.meta.env.MODE === 'development' 
+//   ? "http://localhost:9000"  // Local development
+//   : "https://shayribackend-ff04.onrender.com"; // Production
+
+// console.log('Current mode:', import.meta.env.MODE);
+// console.log('API_URL being used:', API_URL);
+
+// const axiosInstance = axios.create({
+//   baseURL: API_URL,
+//   // headers: {
+//   //   "Content-Type": "application/json",
+//   // },
+//   withCredentials: true,
+// });
+
+// export default axiosInstance;
+
+
+
 import axios from "axios";
 
 // Automatically detect environment
 const API_URL = import.meta.env.MODE === 'development' 
   ? "http://localhost:9000"  // Local development
-  : "https://shayribackend-ff04.onrender.com"; // Production
+  : "http://localhost:4000"; // Production
 
 console.log('Current mode:', import.meta.env.MODE);
 console.log('API_URL being used:', API_URL);
